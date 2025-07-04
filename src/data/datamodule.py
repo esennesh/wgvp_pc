af8c268b77b68e423221616e6ef582d79bdeb3b9
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-07-04 16:39:32 -0500

    src.data.datamodule.DataModule: add drop_last keyword argument as option
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- src/data/datamodule.py
+++ src/data/datamodule.py
@@ -22,7 +22,7 @@ class DataModule:
     """
     Base class for all data modules
     """
-    def __init__(self, batch_size: int=64, data_dir: str="data/",
+    def __init__(self, batch_size: int=64, data_dir: str="data/", drop_last=False,
                  collate_fn=numpy_collate, indexed: bool=False,
                  num_workers: int=1, pin_memory: bool=False, shuffle: bool=True,
                  validation_split: float=0.1):
@@ -38,6 +38,7 @@ class DataModule:
         self.dataloader_kwargs = {
             'batch_size': batch_size,
             'collate_fn': collate_fn,
+            'drop_last': drop_last,
             'num_workers': num_workers,
             'pin_memory': pin_memory,
             'shuffle': shuffle,
