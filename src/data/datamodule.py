3e87cfe95cc9733adc41b72f71a57587540ae471
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-07-28 16:50:22 -0500

    src.data.datamodule.DataMutables: storage class for mutable parameters
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- src/data/datamodule.py
+++ src/data/datamodule.py
@@ -1,8 +1,11 @@
 from abc import abstractmethod, abstractproperty
+from jax import Array
+import jax.numpy as jnp
 import numpy as np
 from jax.tree_util import tree_map
+from pytrie import SortedStringTrie as Trie
 from torch.utils.data import Dataset, DataLoader, default_collate, random_split
-from typing import Tuple
+from typing import Any, Dict, Tuple
 
 def numpy_collate(batch):
   return tree_map(np.asarray, default_collate(batch))
@@ -17,6 +20,60 @@ class IndexedDataset(Dataset):
 
     def __len__(self):
         return len(self._dataset)
+
+class DataMutables:
+    def __init__(self, length, idx_dim=1):
+        self._idx_dim = idx_dim
+        self._length = length
+        self._mutables = Trie()
+
+    def get_parameter(self, idx: np.ndarray, key: str) -> Array:
+        val = np.take(self.mutables[key], idx, axis=self._idx_dim)
+        assert val.shape[self._idx_dim] == len(idx)
+        return jnp.array(val)
+
+    def get_parameters(self, idx: np.ndarray) -> Dict[str, Array]:
+        return {name: self.get_parameter(idx, name) for name in self.mutables}
+
+    def __len__(self):
+        return self._length
+
+    @property
+    def mutables(self):
+        return self._mutables
+
+    def pickle(self):
+        return {
+            "idx_dim": self._idx_dim,
+            "length": len(self),
+            "parameters": {k: v for k, v in self.mutables.items()}
+        }
+
+    def _require(self, key: str, shape: Tuple):
+        assert len(shape) >= 2
+        shape = list(shape)
+        shape[self._idx_dim] = len(self)
+        if key not in self.mutables:
+            self.mutables[key] = np.empty(tuple(shape))
+
+    def set_parameter(self, idx: np.ndarray, key: str, val: Array):
+        self._require(key, val.shape)
+        indices = idx.reshape((1, len(idx)) +\
+                              (1,) * len(val.shape[self._batch_dim+1:]))
+        np.put_along_axis(self.mutables[key],
+                          np.broadcast_to(indices, val.shape),
+                          np.array(val), self._batch_dim)
+
+    def set_parameters(self, idx: np.ndarray, parameters: Dict[str, Array]):
+        for key, val in parameters.items():
+            self.set_parameter(idx, key, val)
+
+    @classmethod
+    def unpickle(cls, saved):
+        self = cls(saved["length"], idx_dim=saved["idx_dim"])
+        for k, v in saved["parameters"].items():
+            self.mutables[k] = v
+        return self
 
 class DataModule:
     """
