1886238f72d741d920ff918d5a8eaa8d56872746
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-07-30 12:27:30 -0500

    Revert "src.trainer.Trainer: always initialize via setup_step() in test()"
    This reverts commit 66c16648c9c12f976fd3ef333781114bdbf77844.

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -152,9 +152,10 @@ class Trainer:
                      datamodule.test_dataloader()
         mutables = datamodule.train_mutables if valid else\
                    datamodule.test_mutables
-        for batch in dataloader:
-            monad.setup_step(*batch)
-            break
+        if ckpt_path is None:
+            for batch in dataloader:
+                monad.setup_step(*batch)
+                break
 
         metrics = defaultdict(lambda: [])
         for batch_idx, batch in enumerate(dataloader):
