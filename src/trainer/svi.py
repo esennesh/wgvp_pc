da4982ff7a5f00b8d8220599fa63ca4cad25faa1
Eli Sennesh <elisennesh@gmail.com>
2025-11-21 22:45:18 -0500

    src.trainer.svi.SviPara: take kwargs to every test, train, and valid step
    Signed-off-by: Eli Sennesh <elisennesh@gmail.com>

--- src/trainer/svi.py
+++ src/trainer/svi.py
@@ -66,14 +66,14 @@ class SviPara(ParaMonad):
     def svi_update(svi, state, data):
         return svi.update(state, data)
 
-    def test_step(self, data, *args):
+    def test_step(self, data, *args, **kwargs):
         self.svi_state, loss = self.svi_evaluate(self.svi, self.svi_state, data)
         return {"loss": loss}
 
-    def train_step(self, data, *args):
+    def train_step(self, data, *args, **kwargs):
         self.svi_state, loss = self.svi_update(self.svi, self.svi_state, data)
         return {"loss": loss}
 
-    def valid_step(self, data, *args) -> Dict[str, float]:
+    def valid_step(self, data, *args, **kwargs) -> Dict[str, float]:
         self.svi_state, loss = self.svi_evaluate(self.svi, self.svi_state, data)
         return {"loss": loss}
