ee9af20c6cd9af03a63361c075fea590c7d36434
Eli Sennesh <elisennesh@gmail.com>
2026-01-06 16:48:09 -0500

    src.inference.graphical.AdaptiveParticleTracer: adapt guide before tracing
    Signed-off-by: Eli Sennesh <elisennesh@gmail.com>

--- src/inference/graphical.py
+++ src/inference/graphical.py
@@ -1,6 +1,7 @@
 from abc import ABC
 from collections import defaultdict
 from functools import cached_property
+import functools
 import jax
 import jax.numpy as jnp
 import jax.random as random
@@ -10,7 +11,7 @@ from numpyro.infer.util import compute_log_probs, get_importance_trace
 from numpyro._typing import Message
 from numpyro.util import _validate_model, check_model_guide_match
 
-from typing import Dict, Optional
+from typing import Callable, Dict, Optional
 
 def configure_sample(msg: Message, /, **kwargs) -> Dict:
     return kwargs
@@ -332,3 +333,80 @@ class OnlineWeightMixin(VariationalMixin):
 
 class OnlineVarGradTracer(OnlineWeightMixin, VarGradTracer):
     pass
+
+class AdaptiveParticleTracer(IwaeMixin, ParticleTracer):
+    def __call__(self, rng_key, param_map, particle_params, model, guide,
+                 *args, **kwargs):
+        param_map = param_map.copy()
+        particle_params = jax.tree.map(
+            lambda leaf: jnp.broadcast_to(leaf, (self.num_particles,
+                                                 *leaf.shape))
+                         if leaf.shape[0] != self.num_particles else leaf,
+            particle_params
+        )
+        if hasattr(guide, "adapt") and isinstance(guide.adapt, Callable):
+            adaptation_rng, rng_key = random.split(rng_key)
+
+            adapt = numpyro.handlers.seed(guide.adapt, adaptation_rng)
+            with numpyro.handlers.substitute(data=param_map):
+                adapted_params = adapt(*args, **kwargs)
+            guide = functools.partial(guide, adaptation=adapted_params)
+
+        def single_trace(rng_key, pwise_params, particle=None):
+            import functools
+
+            param_map.update(pwise_params)
+            particle_guide, particle_model = guide, model
+
+            model_seed, guide_seed = random.split(rng_key)
+            if particle is not None:
+                particle_guide = numpyro.handlers.infer_config(
+                    particle_guide,
+                    functools.partial(configure_sample, k=particle)
+                )
+                particle_model = numpyro.handlers.infer_config(
+                    particle_model,
+                    functools.partial(configure_sample, k=particle)
+                )
+            seeded_model = numpyro.handlers.seed(particle_model, model_seed)
+            seeded_guide = numpyro.handlers.seed(particle_guide, guide_seed)
+            model_trace, guide_trace = get_importance_trace(seeded_model,
+                                                            seeded_guide, args,
+                                                            kwargs, param_map)
+
+            check_model_guide_match(model_trace, guide_trace)
+            _validate_model(model_trace, plate_warning="loose")
+
+            graph_state = {
+                name: (site["value"], site["log_prob"],
+                       guide_trace[name]["log_prob"] if name in guide_trace\
+                       else jnp.zeros_like(site["log_prob"]),
+                       site["is_observed"])
+                for name, site in model_trace.items()
+                if site["type"] == "sample"
+            }
+            graph_state.update({
+                name: (site["value"], jnp.zeros_like(site["log_prob"]),
+                       site["log_prob"], False)
+                      for name, site in guide_trace.items()
+                      if site["type"] == "sample" and name not in graph_state
+            })
+            graph_state.update({
+                name: (site["value"], 0., 0., False)
+                for name, site in model_trace.items()
+                if site["type"] == "deterministic"
+            })
+            graph_state.update({
+                name: (site["value"], 0., 0., False)
+                for name, site in guide_trace.items()
+                if site["type"] == "deterministic"
+            })
+            mutables = {name: site["value"] for name, site in
+                        model_trace.items() if site["type"] == "mutable"}
+
+            return graph_state, mutables
+
+        rng_keys = random.split(rng_key, self.num_particles)
+        particles = jnp.arange(self.num_particles)
+        particle_traces = jax.vmap(single_trace)
+        return particle_traces(rng_keys, particle_params, particle=particles)
