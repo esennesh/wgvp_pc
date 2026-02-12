93f9cafb1d9fcaade25fd5808689ce97ee68e0e9
Eli Sennesh <elisennesh@gmail.com>
2026-02-12 15:59:11 -0500

    src.utils.util: bring in Hadi's filter_kwargs() function
    Signed-off-by: Eli Sennesh <elisennesh@gmail.com>

--- src/utils/util.py
+++ src/utils/util.py
@@ -5,6 +5,7 @@ from jax.example_libraries.optimizers import (OptimizerState,
                                               unpack_optimizer_state)
 import json
 from importlib.util import find_spec
+import inspect
 import logging
 import numpy as np
 import numpyro
@@ -23,6 +24,20 @@ import rich.tree
 from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple
 
 log = logging.LoggerAdapter(logger=logging.getLogger(__name__))
+
+def filter_kwargs(fn, kw=None):
+    if not kw:
+        return {}
+    try:
+        if isinstance(fn, type):  # class
+            params = get_all_init_params(fn)
+        elif callable(fn):  # function
+            params = inspect.signature(fn).parameters
+        else:
+            raise ValueError(type(fn).__name__)
+        return {k: v for k, v in kw.items() if k in params}
+    except ValueError:
+        return kw
 
 def sort_key(key):
     match = re.match(r"(\d+)(\+?)", key)
